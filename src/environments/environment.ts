d2b2a8aa7988d57a245fe2ee8fa61deccaa0daa3
Youssef OUMENSKOU <youmenskou@EILCALAIS.FR>
2023-10-02 08:37:08 +0100

    init commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment={
+  firebase: {
+    projectId: 'surveillance-reservoir',
+    appId: '1:425229845024:web:a8b49c938e7f7a77b7f245',
+    storageBucket: 'surveillance-reservoir.appspot.com',
+    apiKey: 'AIzaSyC_abYkNEzpEbvo722Uo5n77xuK-tAljh0',
+    authDomain: 'surveillance-reservoir.firebaseapp.com',
+    messagingSenderId: '425229845024',
+  },
+    production: false
+};
